c67d0d310bb68c77afc80338dfbf99b464f7305b
thielen4 <jenny.thielen@kellogg.northwestern.edu>
2020-02-25 02:53:47 +0000

    class notes

--- app.rb
+++ app.rb
@@ -14,3 +14,32 @@ after { puts; }                                                             #
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+
+
+get "/" do
+    puts events_table.all
+    @events = events_table.all
+    #this creates a variable that is the array of hashes "events"
+    view "events"
+end
+
+
+get "/events/:id" do
+        #:id placeholder for actual event id
+        #this is a definition of a url pattern that the application is looking for
+    @event = events_table.where(id: params[:id]).first
+        #to_s converts to string vs. ugly ruby version
+        #first gives back first entry in array of Sequl SQLite table
+        #now we have a hash. we grab its keys & values
+    view "event"
+        #want to show an "event" view, so have to create a new event.erb view file
+end
+
+
+get "events/:id/rsvps/new" do 
+    #this is a form that's creating an RSVP for a specific event
+    @event = events_table.where(id: params[:id]).first
+    # this is how we get the event!
+    view new_rsvp
+end
+
